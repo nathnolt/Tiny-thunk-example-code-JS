f3345b6c6007496c57c99608493c87191d226a24
nathnolt <nathnolt@gmail.com>
2020-07-09 14:35:27 +0200

    Make it generic, so it works for any callback style function.

--- main.js
+++ main.js
@@ -2,12 +2,18 @@
 //-------------------------------------
 // user code of the thunkified request
 //-------------------------------------
+
+
+var thunkRequest = thunkify(getRequest)
+
+
 function mainCode() {
 	
 	var beforeTime = Date.now()
 	
 	var thunkGetUser1 = thunkRequest('users/user1', 'get')
 	var thunkGetUser2 = thunkRequest('users/user2', 'post')
+	
 	
 	thunkGetUser1(function user1Got(user1) {
 		thunkGetUser2(function user2Got(user2) {
@@ -16,6 +22,7 @@ function mainCode() {
 			console.log('time took', afterTime - beforeTime, user1.ms, user2.ms)
 		})
 	})
+	
 }
 
 mainCode()
@@ -28,6 +35,13 @@ mainCode()
 //---------------------------
 // thunkified reqyest thingy
 //---------------------------
+
+
+
+
+/*
+	Intermediate function I wrote first, in order to write the thunkify function
+
 function thunkRequest(url, method) {
 	var userFnPtr = null
 	var data = null
@@ -49,7 +63,50 @@ function thunkRequest(url, method) {
 	
 	return returnFn
 }
+*/
 
+function newArr(arr) {
+	return Array.prototype.slice.call(arr, 0)
+}
+
+function thunkify(callbackFn) {
+	
+	var userFnPointer = null
+	var callbackResult = null
+	
+	var callbackFnCallbackFn
+	var userCallable
+	
+	callbackFnCallbackFn = function thunkifiedCallback(result) {
+		if(userFnPointer != null) {
+			userFnPointer(result)
+		} else {
+			callbackResult = result
+		}
+	}
+	
+	
+	userCallable = function userCallable(userFn) {
+		if(callbackResult != null) {
+			userFn(callbackResult)
+		} else {
+			userFnPointer = userFn
+		}
+	}
+	
+	
+	
+	var thunkifiedFn = function thunkified() {
+		var argsArr = newArr(arguments)
+		var fixedArgs = argsArr.concat(callbackFnCallbackFn)
+		
+		callbackFn.apply(this, fixedArgs)
+		
+		return userCallable
+	}
+	
+	return thunkifiedFn
+}
 
 
 
